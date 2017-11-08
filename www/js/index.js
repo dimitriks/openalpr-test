7572c12141919cfea032c71830e9f51164034e12
Kucher <kucher@buhphone.com>
2017-11-08 23:36:12 +0200

    main methods

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -21,13 +21,9 @@ var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
 
-        // function  onClickOpenFileButton() {
-        //     alert('onClickOpenFileButton');
-        // }
+        var openFileButton = document.getElementsByClassName("open-file-button");
 
-        var onClickOpenFileButton = document.getElementsByClassName("open-file-button");
-
-        onClickOpenFileButton[0].addEventListener("click", this.onClickOpenFileButton, false);
+        openFileButton[0].addEventListener("click", this.onClickOpenFileButton.bind(this), false);
     },
 
     // deviceready Event Handler
@@ -40,20 +36,86 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        // var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+        //
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
     },
 
     onClickOpenFileButton: function() {
-        alert('onClickOpenFileButton');
-    }
+        console.log('OpenFileButton Click');
 
+        this.cameraGetPicture();
+    },
+
+    cameraGetPicture: function () {
+        var cameraOptions = {
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 1024, targetHeight: 1024
+        };
+
+        navigator.camera.getPicture(this.cameraSuccess.bind(this), this.cameraError.bind(this), cameraOptions);
+    },
+
+    cameraSuccess: function (url) {
+        console.log('photo loaded...');
+        console.log(url);
+
+        if (device.platform === "Android") {
+            this.resoolveNativePathAndThenOpenALPRScan(url);
+        } else {
+            this.openALPRScan(url);
+        }
+
+        return false;
+    },
+
+    cameraError: function (e) {
+        console.log('Error photo loading: ', e);
+
+        return false;
+    },
+
+    openALPRScan: function (url) {
+        var resultText = document.getElementsByClassName('result-text')[0];
+        var resultMessage = 'data not recognized';
+
+        function successScan(data) {
+            if (data.length) {
+                resultMessage = JSON.stringify(data);
+            }
+
+            resultText.value = resultMessage;
+            console.log(resultMessage);
+        }
+
+        function errorScan(error) {
+            resultMessage = error.code + ': ' + error.message;
+            resultText.value = resultMessage;
+
+            console.log(resultMessage);
+        }
+
+        cordova.plugins.OpenALPR.scan(url, successScan, errorScan);
+    },
+
+    resoolveNativePathAndThenOpenALPRScan: function (url) {
+        function onSuccess(result) {
+            this.openALPRScan(result);
+        }
+
+        function onError(error) {
+            logger.debug("resolveNativePath error", error);
+        }
+
+        window.FilePath.resolveNativePath(url, onSuccess.bind(this), onError.bind(this));
+    }
 };
 
 app.initialize();
